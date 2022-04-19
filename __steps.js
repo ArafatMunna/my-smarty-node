8834fd5dcc0e08fdc324252041b9b5d47eaefba2
ArafatMunna <arafatmunna000@gmail.com>
2022-04-20 00:40:47 +0600

    steps added

---
+++ __steps.js
@@ -0,0 +1,27 @@
+/* 
+    --------------------------------------
+    Basic Server Setup
+    --------------------------------------
+    1. Create a folder (manually or mkdir)
+    2. Open command line to that folder
+    3. npm init
+    4. npm install express
+    5. Create index.js
+    --------------------------------------
+    For Express/node server
+    --------------------------------------
+    1. require express
+    2. Create app variable
+    3. declare port
+    4. set app.get("/")
+    5. listen to port
+    6. node index.js
+    7. check your browser for that port
+    --------------------------------------
+    Get
+    --------------------------------------
+    1. Cors middleware
+    --------------------------------------
+    Post
+    --------------------------------------
+*/
